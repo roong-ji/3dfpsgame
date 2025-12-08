725bf36a71423d08850d3f9c269bfb7baaf4a684
roong-ji <mhienegjyuu@naver.com>
2025-12-08 17:38:06 +0900

    feat: 플레이어 중력 적용

--- Assets/02.Scripts/Player/PlayerMove.cs
+++ Assets/02.Scripts/Player/PlayerMove.cs
@@ -5,6 +5,10 @@ public class PlayerMove : MonoBehaviour
 {
     private float _moveSpeed = 7f;
 
+    private const float Gravity = -9.81f;
+    private float _yVelocity = 0;
+    private float _jumpPower = 5f;
+
     private CharacterController _controller;
 
     private void Awake()
@@ -14,11 +18,21 @@ public class PlayerMove : MonoBehaviour
 
     private void Update()
     {
+        _yVelocity += Gravity * Time.deltaTime;
+
         float x = Input.GetAxis("Horizontal");
         float y = Input.GetAxis("Vertical");
 
         Vector3 direction = new Vector3(x, 0, y);
+
+        // if (_controller.collisionFlags == CollisionFlags.Below)
+        if (Input.GetButtonDown("Jump") && _controller.isGrounded)
+        {
+            _yVelocity = _jumpPower;
+        }
+
         direction = transform.TransformDirection(direction);
+        direction.y = _yVelocity;
 
         _controller.Move(motion: direction * _moveSpeed * Time.deltaTime);
     }

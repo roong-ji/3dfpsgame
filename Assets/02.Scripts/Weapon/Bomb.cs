19fca48abdbd9780bea07a1a253adcebbbaac15b
roong-ji <mhienegjyuu@naver.com>
2025-12-09 14:22:25 +0900

    asset: 폭발 파티클

---
+++ Assets/02.Scripts/Weapon/Bomb.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class Bomb : MonoBehaviour
+{
+    [SerializeField] private GameObject _explosionEffectPrefab;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Instantiate(_explosionEffectPrefab, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+}

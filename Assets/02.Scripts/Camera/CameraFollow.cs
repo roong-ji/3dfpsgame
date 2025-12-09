f807690db3932f1d0f5567cf061c57f594e6603f
roong-ji <mhienegjyuu@naver.com>
2025-12-09 11:20:17 +0900

    feat: 카메라 시점 변환 추가

--- Assets/02.Scripts/Camera/CameraFollow.cs
+++ Assets/02.Scripts/Camera/CameraFollow.cs
@@ -1,11 +1,44 @@
 using UnityEngine;
+using DG.Tweening;
 
 public class CameraFollow : MonoBehaviour
 {
-    [SerializeField] private Transform _target;
+    [SerializeField] private Transform[] _targets;
+    private Transform _currentTarget;
+    private int _targetIndex = 0;
+    private float _transitionDuration = 1f;
+    [SerializeField] private Ease _easeType = Ease.Linear;
+    private bool _isChanging = false;
+
+    private void Start()
+    {
+        _currentTarget = _targets[_targetIndex];
+    }
+
+    private void Update()
+    {
+        InputChangeTarget();
+    }
 
     private void LateUpdate()
     {
-        transform.position = _target.position;
+        if( _isChanging) return;
+        transform.position = _currentTarget.position;
+    }
+
+    private void InputChangeTarget()
+    {
+        if (!Input.GetKeyDown(KeyCode.T)) return;
+
+        _targetIndex = (_targetIndex + 1) % _targets.Length;
+        _currentTarget = _targets[_targetIndex];
+        _isChanging = true;
+
+        transform.DOMove(_currentTarget.position, _transitionDuration)
+            .SetEase(_easeType)
+            .OnComplete(() =>
+            {
+                _isChanging = false;
+            });
     }
 }

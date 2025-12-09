71ca3c97d897b937becf554765c9aff8f02f88fc
roong-ji <mhienegjyuu@naver.com>
2025-12-09 10:56:02 +0900

    feat: 스태미나를 소모하는 더블 점프 추가

--- Assets/02.Scripts/Player/PlayerMove.cs
+++ Assets/02.Scripts/Player/PlayerMove.cs
@@ -11,6 +11,8 @@ public class PlayerMove : MonoBehaviour
     private const float Gravity = -9.81f;
     private float _yVelocity = 0;
     private float _jumpPower = 5f;
+    private float _duobleJumpStaminaCost = 50f;
+    private bool _canDoubleJump = false;
 
     private CharacterController _controller;
     private PlayerStamina _stamina;
@@ -24,29 +26,41 @@ public class PlayerMove : MonoBehaviour
 
     private void Update()
     {
+        HandleJump();
         HandleMoveSpeed();
         HandleMovement();
     }
 
     private void HandleMovement()
     {
-        _yVelocity += Gravity * Time.deltaTime;
-
         float x = Input.GetAxis("Horizontal");
         float y = Input.GetAxis("Vertical");
 
         Vector3 direction = new Vector3(x, 0, y);
 
-        // if (_controller.collisionFlags == CollisionFlags.Below)
-        if (Input.GetButtonDown("Jump") && _controller.isGrounded)
-        {
-            _yVelocity = _jumpPower;
-        }
-
         direction = transform.TransformDirection(direction);
         direction.y = _yVelocity;
 
         _controller.Move(motion: direction * _currentMoveSpeed * Time.deltaTime);
+    }
+
+    private void HandleJump()
+    {
+        _yVelocity += Gravity * Time.deltaTime;
+
+        // if (_controller.collisionFlags == CollisionFlags.Below)
+        if (!Input.GetButtonDown("Jump")) return;
+
+        if (_controller.isGrounded)
+        {
+            _yVelocity = _jumpPower;
+            _canDoubleJump = true;
+        }
+        else if (_canDoubleJump && _stamina.TryUseStamina(_duobleJumpStaminaCost))
+        {
+            _yVelocity = _jumpPower;
+            _canDoubleJump = false;
+        }
     }
 
     private void HandleMoveSpeed()

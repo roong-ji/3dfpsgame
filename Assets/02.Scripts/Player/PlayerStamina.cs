f528b874295a9260301b05fdc16c679f145982e2
roong-ji <mhienegjyuu@naver.com>
2025-12-09 09:34:18 +0900

    feat: 스태미나 추가

---
+++ Assets/02.Scripts/Player/PlayerStamina.cs
@@ -0,0 +1,42 @@
+using System;
+using UnityEngine;
+
+public class PlayerStamina : MonoBehaviour
+{
+    [Header("최대 스태미나")]
+    [SerializeField] private float _maxStamina = 100f;
+    private float _stamina;
+
+    [Header("초당 스태미나 회복량")]
+    [SerializeField] private float _staminaRegenRate = 1f;
+
+    public event Action<float, float> OnStaminaChanged;
+
+    public float Stamina
+    {
+        get { return _stamina; } 
+        private set 
+        { 
+            _stamina = value;
+            OnStaminaChanged?.Invoke(_stamina, _maxStamina);
+        }
+    }
+
+    private void Update()
+    {
+        RegenStamina();
+    }
+
+    private void RegenStamina()
+    {
+        _stamina += _staminaRegenRate * Time.deltaTime;
+    }
+
+    public bool UseStamina(float stamina)
+    {
+        if(stamina <=0 || _stamina < stamina) return false;
+
+        Stamina -= stamina;
+        return true;
+    }
+}

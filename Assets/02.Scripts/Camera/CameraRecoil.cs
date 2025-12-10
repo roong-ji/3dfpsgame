cc232b70138adf07a207ad93bec5cdd93f8c1ddc
roong-ji <mhienegjyuu@naver.com>
2025-12-10 15:15:00 +0900

    feat: 반동이 점진적으로 증가하도록 수정

--- Assets/02.Scripts/Camera/CameraRecoil.cs
+++ Assets/02.Scripts/Camera/CameraRecoil.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using UnityEngine;
 
 public class CameraRecoil : MonoBehaviour
@@ -5,15 +6,17 @@ public class CameraRecoil : MonoBehaviour
     private float _recoilMinX = 2.5f;
     private float _recoilMaxX = 5f;
     private float _recoilY = 0.5f;
-    private float _returnSpeed = 5f;
+
+    private float _startReturnDuration = 0.2f;
+    private float _currentReturnDuration = 0.2f;
+    private float _durationIncrement = 0.1f;
+    private float _maxReturnDuration = 1f;
 
     private Vector3 _recoilOffset;
     public Vector3 RecoilOffset => _recoilOffset;
 
-    private void Update()
-    {
-        _recoilOffset = Vector3.Lerp(_recoilOffset, Vector3.zero, _returnSpeed * Time.deltaTime);
-    }
+    private Tweener _recoilTweener;
+    private Ease _returnEase = Ease.OutQuad;
 
     public void CameraRecoilByFire()
     {
@@ -21,5 +24,19 @@ public class CameraRecoil : MonoBehaviour
         float y = Random.Range(-_recoilY, _recoilY);
 
         _recoilOffset += new Vector3(x, y, 0);
+
+        _currentReturnDuration += _durationIncrement;
+        _currentReturnDuration = Mathf.Min(_currentReturnDuration, _maxReturnDuration);
+
+        _recoilTweener.Kill();
+        _recoilTweener = DOTween.To(
+            () => _recoilOffset,
+            val => _recoilOffset = val,
+            Vector3.zero,
+            _currentReturnDuration
+        )
+        .SetEase(_returnEase)
+        .OnComplete(() => _currentReturnDuration = _startReturnDuration);
+
     }
 }

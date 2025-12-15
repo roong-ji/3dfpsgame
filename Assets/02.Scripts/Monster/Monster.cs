03fb9296ae393bf1ce7ddd502d4a92b484e8a5ed
roong-ji <mhienegjyuu@naver.com>
2025-12-15 16:05:43 +0900

    Reapply "feat: PlayerStats 싱글톤 해제"
    This reverts commit 227e48fca27b365f4f3b3f3d03ad6bec736fd9c1.

--- Assets/02.Scripts/Monster/Monster.cs
+++ Assets/02.Scripts/Monster/Monster.cs
@@ -6,7 +6,7 @@ public class Monster : MonoBehaviour, IDamagable
     private Dictionary<EMonsterState, BaseState> _states;
     private BaseState _state;
 
-    private GameObject _player;
+    [SerializeField] private GameObject _player;
     private CharacterController _controller;
 
     public ConsumableStat Health;
@@ -46,7 +46,6 @@ public class Monster : MonoBehaviour, IDamagable
     {
         Health.Initialize();
 
-        _player = PlayerStats.Instance.gameObject;
         _controller = GetComponent<CharacterController>();
 
         _states = new Dictionary<EMonsterState, BaseState>
